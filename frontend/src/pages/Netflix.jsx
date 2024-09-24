cc9059fd74ac8c781ca7731d54291ac8a755c481
RatnagiriRG <ratnagiri557@gmail.com>
2024-09-24 11:40:56 +0530

    dd

--- frontend/src/pages/Netflix.jsx
+++ frontend/src/pages/Netflix.jsx
@@ -1,9 +1,13 @@
-    import React from 'react'
-    
-    function Netflix() {
-      return (
-        <div>Netflix</div>
-      )
-    }
-    
-    export default Netflix
+import React, { useState } from "react";
+
+function Netflix() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  return <div>Netflix</div>;
+}
+
+export default Netflix;
